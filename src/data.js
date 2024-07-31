c2b4c77209b48f42d70a5fdbe90310951bf41c4d
Chandra Lama <iamme24cl@gmail.com>
2024-07-30 20:42:37 -0700

    add file to hold store data

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2016/07/28/10/45/woman-1547507_1280.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF ON ALL NEW ARRIVALS!",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/902030/pexels-photo-902030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF ON ALL NEW ARRIVALS!",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/2065195/pexels-photo-2065195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title: "MOST POPULAR",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF ON ALL NEW ARRIVALS!",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2016/07/28/10/45/woman-1547507_1280.jpg",
+        title: "SUMMER SALE",
+    },
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2016/07/28/10/45/woman-1547507_1280.jpg",
+        title: "SUMMER SALE",
+    },
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2016/07/28/10/45/woman-1547507_1280.jpg",
+        title: "SUMMER SALE",
+    },
+];
