ecc297dc913b9af0f5c1ce68dbf7090a918d3545
Chandra Lama <iamme24cl@gmail.com>
2024-07-31 18:27:50 -0700

    add product and products components

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,13 +6,15 @@ import Product from './Product';
 const Container = styled.div`
     padding: 20px;
     display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 `;
 
 const Products = () => {
   return (
     <Container>
         {popularProducts.map((item) => (
-            <Product product={item} key={item.id} />
+            <Product item={item} key={item.id} />
         ))}
     </Container>
   );
